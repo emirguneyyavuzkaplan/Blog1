e42cee38067252fb7d35ab69568d08745cd4f8e8
emirguneyyavuzkaplan <103495760+emirguneyyavuzkaplan@users.noreply.github.com>
2022-11-14 20:08:05 +0300

    Initial commit

---
+++ app/Http/Controllers/Front/HomepageController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Front;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+
+class HomepageController extends Controller
+{
+
+}
