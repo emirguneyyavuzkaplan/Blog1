efde39d767ec74ec90455251b910e0d96555c9dc
emirguneyyavuzkaplan <103495760+emirguneyyavuzkaplan@users.noreply.github.com>
2022-12-11 23:41:49 +0300

    proje son hali

--- resources/views/Back/articles/index.blade.php
+++ resources/views/Back/articles/index.blade.php
@@ -1,11 +1,11 @@
 @extends('back.layouts.master')
 @section('title','Tüm Makaleler')
 @section('content')
-    <div class="card shadow mb-4" xmlns="http://www.w3.org/1999/html">
+    <div class="card shadow mb-4">
         <div class="card-header py-3">
             <h6 class="m-0 font-weight-bold text-primary">@yield('title')
-                <span class="float-right">{{$articles->count()}} makale bulundu.</span>
-                <a href="{{route('admin.trashed')}}" class="btn btn-warning btn-sm"><i class="fa fa-trash"></i>  Silinen makaleler</a>
+                <span class="float-right">{{$articles->count()}} makale bulundu.</strong>
+                    <a href="{{route('admin.trashed')}}" class="btn btn-warning btn-sm"><i class="fa fa-trash"></i> Silinen Makaleler</a>
             </h6>
         </div>
         <div class="card-body">
@@ -13,35 +13,34 @@
                 <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                     <thead>
                     <tr>
-                        <th>Fotoğraflar</th>
-                        <th>Makale Baslıgı</th>
+                        <th>Fotoğraf</th>
+                        <th>Makale Başlığı</th>
                         <th>Kategori</th>
                         <th>Hit</th>
-                        <th>Olusturulma Tarihi</th>
+                        <th>Tarih</th>
                         <th>Durum</th>
                         <th>İşlemler</th>
                     </tr>
                     </thead>
-
                     <tbody>
                     @foreach($articles as $article)
-                        <tr></tr>
-                        <td>
-                            <img src="{{$article->image}}" width="200">
-                        </td>
-                        <td>{{$article->title}}</td>
-                        <td>{{$article->getCategory->name}}</td>
-                        <td>{{$article->hit}}</td>
-                        <td>{{$article->created_at->diffForHumans()}}</td>
-                        <input  id="switch" data="{{$article->id}}" type="checkbox" data-on="Aktif" data-off="Pasif" data-onstyle="success"  data-offstyle="danger" @if($article->status==1) checked @endif data-toggle="toogle">
-
-                        <td>
-                            <a href="{{route('single',[$article->getCategory->slug,$article->slug])}}" title="Görüntüle" class="btn btn-sm btn-success"><i class="fa fa-eye"></i> </a>
-                            <a href="{{route('admin.makaleler.edit',$article->id)}}" title="Düzenle" class="btn btn-sm btn-primary"><i class="fa fa-pencil"></i> </a>
-                            <a href="{{route('admin.delete.article',$article->id)}}" title="Sil" class="btn btn-sm btn-danger"><i class="fa fa-times"></i> </a>
-
-                        </td>
-
+                        <tr>
+                            <td>
+                                <img src="{{asset($article->image)}}" width="200">
+                            </td>
+                            <td>{{$article->title}}</td>
+                            <td>{{$article->getCategory->name}}</td>
+                            <td>{{$article->hit}}</td>
+                            <td>{{$article->created_at->diffForHumans()}}</td>
+                            <td>
+                                <input class="switch" article-id="{{$article->id}}" type="checkbox" data-on="Aktif" data-off="Pasif" data-onstyle="success" data-offstyle="danger" @if($article->status==1) checked
+                                       @endif data-toggle="toggle">
+                            </td>
+                            <td>
+                                <a target="_blank" href="{{route('single',[$article->getCategory->slug,$article->slug])}}" title="Görüntüle" class="btn btn-sm btn-success"><i class="fa fa-eye"></i> </a>
+                                <a href="{{route('admin.makaleler.edit',$article->id)}}" title="Düzenle" class="btn btn-sm btn-primary"><i class="fa fa-pen"></i> </a>
+                                <a href="{{route('admin.delete.article',$article->id)}}" title="Sil" class="btn btn-sm btn-danger"><i class="fa fa-times"></i> </a>
+                            </td>
                         </tr>
                     @endforeach
                     </tbody>
@@ -49,22 +48,19 @@
             </div>
         </div>
     </div>
-
-
 @endsection
 @section('css')
     <link href="https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css" rel="stylesheet">
 @endsection
 @section('js')
     <script src="https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js"></script>
-
-
     <script>
         $(function() {
-            $('#switch').change(function (){
-                alert('Toggle:'+$(this).prop('checked'));
+            $('.switch').change(function() {
+                id = $(this)[0].getAttribute('article-id');
+                statu=$(this).prop('checked');
+                $.get("{{route('admin.switch')}}", {id:id,statu:statu},  function(data, status) {});
             })
         })
     </script>
 @endsection
-
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Front\HomepageController;
 use App\Http\Controllers\Back\Dashboard;
 use App\Http\Controllers\Back\AuthController;
 use App\Http\Controllers\Back\ArticleController;
+use App\Http\Controllers\Back\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,12 +23,23 @@ Route::prefix('admin')->name('admin.')->middleware('isLogin')->group(function ()
 });
 Route::prefix('admin')->name('admin.')->middleware('isAdmin')->group(function (){
     Route::get('panel',[Dashboard::class,'index'])->name('dashboard');
+    //MAKALE ROUTE
     Route::get('makaleler/silinenler',[ArticleController::class,'trashed'])->name('trashed');
     Route::resource('makaleler', ArticleController::class);
     Route::get('/switch',[ArticleController::class,'switch'])->name('switch');
     Route::get('/deletearticle/{id}',[ArticleController::class,'delete'])->name('delete.article');
     Route::get('/harddeletearticle/{id}',[ArticleController::class,'hardDelete'])->name('hard.delete.article');
     Route::get('/recoverarticle/{id}',[ArticleController::class,'recover'])->name('recover.article');
+
+
+    //CATEGORİ ROUTE
+    Route::get('/kategoriler',[CategoryController::class,'index'])->name('category.index');
+    Route::post('/kategoriler/create',[CategoryController::class,'create'])->name('category.create');
+    Route::post('/kategoriler/update',[CategoryController::class,'update'])->name('category.update');
+    Route::post('/kategoriler/delete',[CategoryController::class,'delete'])->name('category.delete');
+    Route::get('/kategori/status',[CategoryController::class,'switch'])->name('category.switch');
+    Route::get('/kategori/getData',[CategoryController::class,'getData'])->name('category.getdata');
+    //
     Route::get('cikis',[AuthController::class,'logout'])->name('logout');
 
 
